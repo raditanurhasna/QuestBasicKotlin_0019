125e6cac773bf7f84c52fae717ce6879a8664f4f
raditanurhasna <raditanurhasna1410@gmail.com>
2024-10-11 14:49:13 +0700

    Menambahkan pengecekan untuk variabel yang dapat bernilai null dan memperbaiki cara penanganan nilai null.

--- app/src/main/java/com/example/questbasickotlin_0019/NullSafety.kt
+++ app/src/main/java/com/example/questbasickotlin_0019/NullSafety.kt
@@ -2,4 +2,16 @@ package com.example.questbasickotlin_0019
 
 fun nullSafety() {
     var neverNull: String = "This can't be null"
+
+    var nullable: String? = "You can keep a null here"
+    nullable = null
+
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("infereedNonNull is not null")
+    }
+
+
 }
+
