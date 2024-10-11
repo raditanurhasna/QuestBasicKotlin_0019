2f146b35c4978ec0fe88dd19638a119267bfdd8b
raditanurhasna <raditanurhasna1410@gmail.com>
2024-10-11 10:05:58 +0700

    Tipe data dasar pada kotlin dan menampilkan datanya

--- app/src/main/java/com/example/questbasickotlin_0019/Tipedata.kt
+++ app/src/main/java/com/example/questbasickotlin_0019/Tipedata.kt
@@ -1,5 +1,43 @@
 package com.example.questbasickotlin_0019
 
 fun main() {
+    //Byte
+    val byteValue: Byte = 18
+    println("Byte: $byteValue")
+
+    //Short
+    val shortValue : Short = 20
+    println("Short: $shortValue")
+
+    //Int
+    val IntValue : Int = 30
+    println("Int : $IntValue")
+
+    //Long
+    val LongValue : Long = 40
+    println("long : $LongValue")
+
+    //Float
+    val FloatValue : Float = 50.6f
+    println("float : $FloatValue")
+
+    //Double
+    val DoubleValue : Double = 60.6
+    println("Double: $DoubleValue")
+
+    //Char
+    val charValue : Char = 'A'
+    println(charValue)
+
+    //Boolean
+    val BooleanValue: Boolean = true
+    println("kotlin: $BooleanValue")
+
+    //String
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin"
+    println("string: $stringValue")
 
 }
