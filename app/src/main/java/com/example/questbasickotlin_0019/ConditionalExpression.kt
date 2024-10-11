2c3adb07ae29a7826b20f35450b86d6bbebfad54
raditanurhasna <raditanurhasna1410@gmail.com>
2024-10-11 13:00:09 +0700

    Menambahkan fungsi untuk mencetak nama hari berdasarkan nilai inpu.

--- app/src/main/java/com/example/questbasickotlin_0019/ConditionalExpression.kt
+++ app/src/main/java/com/example/questbasickotlin_0019/ConditionalExpression.kt
@@ -9,4 +9,18 @@ fun ConditionalStatement() {
     } else {
         println("Negative Number")
     }
+
+    val day = 1
+    when (day) {
+        1 ->  println("Sunday")
+        2 ->  println("Monday")
+        3 ->  println("Tuesday")
+        4 ->  println("Wednesday")
+        5 ->  println("Thursday")
+        6 ->  println("Friday")
+        7 ->  println("Saturday")
+        else -> println("Invalid Day")
+    }
+
+
 }
