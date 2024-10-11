4f10992d38701850eb05ba984435c76edf964b1f
raditanurhasna <raditanurhasna1410@gmail.com>
2024-10-11 12:56:20 +0700

    Digunakan untuk pernyataan kondisional untuk memeriksa apakah angka positif atau negati

---
+++ app/src/main/java/com/example/questbasickotlin_0019/ConditionalExpression.kt
@@ -0,0 +1,12 @@
+package com.example.questbasickotlin_0019
+
+fun ConditionalStatement() {
+    println("=== Condtional Expression ===")
+
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+}
