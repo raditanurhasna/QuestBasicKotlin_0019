ba37fd25dd93ac09a2c204e1891b9acbd9542356
raditanurhasna <raditanurhasna1410@gmail.com>
2024-10-11 12:32:40 +0700

    Digunakan untuk menambahkan data, menghapus data dan mengubah data dalam List Mutable

--- app/src/main/java/com/example/questbasickotlin_0019/Collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0019/Collections.kt
@@ -5,4 +5,16 @@ fun ContohList() {
 
     val readOnlyAbjad = listOf("A", "B", "C")
     println(readOnlyAbjad)
+
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    shape.remove("Triangle")
+    println(shape)
+
+    shape[0] = "oval"
+    println(shape)
+
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
 }
