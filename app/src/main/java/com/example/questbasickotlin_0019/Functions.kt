a3ed27b0fc5cd795c76c4f8da590333dfb88e065
raditanurhasna <raditanurhasna1410@gmail.com>
2024-10-11 14:33:15 +0700

    Menambahkan fungsi untuk menyapa pengguna dengan parameter, argumen bernama, dan parameter default.

--- app/src/main/java/com/example/questbasickotlin_0019/Functions.kt
+++ app/src/main/java/com/example/questbasickotlin_0019/Functions.kt
@@ -1,6 +1,29 @@
 package com.example.questbasickotlin_0019
 
-fun witthoutPrameter() {
+fun withoutParameter() {
     println("== withoutParameter ==")
     println("Hello, World!")
 }
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age year old.")
+}
+
+fun withReturn(panjang : Int, lebar : Int) : Int {
+    return println(panjang * lebar)
+}
+
